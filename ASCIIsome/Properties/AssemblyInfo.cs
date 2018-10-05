1b7eebdde791099ef65168d2e2256e09bd337c2b
Hafniz <42380178+Hafniz@users.noreply.github.com>
2018-10-05 17:52:32 +0800

    Added basic localization framework

--- ASCIIsome/Properties/AssemblyInfo.cs
+++ ASCIIsome/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ASCIIsome")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Hafniz 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
