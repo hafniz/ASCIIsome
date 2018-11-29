9bb04ca2322387cbbb74da92993860760a0e22f0
hafniz <42380178+hafniz@users.noreply.github.com>
2018-11-29 13:30:37 +0800

    a0.0.36.5: Added support for Per-Monitor DPI Awareness v2

--- ASCIIsome/ApplicationInfo.cs
+++ ASCIIsome/ApplicationInfo.cs
@@ -6,7 +6,7 @@ namespace ASCIIsome
     public static class ApplicationInfo
     {
         public static string ApplicationName { get; } = "ASCIIsome";
-        public static Version ApplicationVersion { get; } = new Version(0, 0, 34, 30); // TODO: [HV] Automatically generate (and format) version info in ApplicationInfo and AssemblyInfo class on building/publishing 
+        public static Version ApplicationVersion { get; } = new Version(0, 0, 36, 5); // TODO: [HV] Automatically generate (and format) version info in ApplicationInfo and AssemblyInfo class on building/publishing 
         public static string VersionPrefix { get; } = "a";
         public static string VersionSuffix { get; } = "_181020-1825";
         public static SolidColorBrush ApplicationTitleBrush { get; } = new SolidColorBrush(Color.FromRgb(43, 87, 151));
