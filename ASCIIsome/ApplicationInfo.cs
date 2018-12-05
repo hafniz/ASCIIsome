3306206847aeaef4a5338d058b185141d90ffb40
hafniz <42380178+hafniz@users.noreply.github.com>
2018-12-05 18:47:06 +0800

    a0.0.37.6: Re-targeted from .NET Framework 4.7 to 4.7.2
    Signed-off-by: hafniz <42380178+hafniz@users.noreply.github.com>

--- ASCIIsome/ApplicationInfo.cs
+++ ASCIIsome/ApplicationInfo.cs
@@ -6,9 +6,9 @@ namespace ASCIIsome
     public static class ApplicationInfo
     {
         public static string ApplicationName { get; } = "ASCIIsome";
-        public static Version ApplicationVersion { get; } = new Version(0, 0, 36, 5); // TODO: [HV] Automatically generate (and format) version info in ApplicationInfo and AssemblyInfo class on building/publishing 
+        public static Version ApplicationVersion { get; } = new Version(0, 0, 37, 6); // TODO: [HV] Automatically generate (and format) version info in ApplicationInfo and AssemblyInfo class on building/publishing 
         public static string VersionPrefix { get; } = "a";
-        public static string VersionSuffix { get; } = "_181020-1825";
+        public static string VersionSuffix { get; } = "_181205-1845";
         public static SolidColorBrush ApplicationTitleBrush { get; } = new SolidColorBrush(Color.FromRgb(43, 87, 151));
     }
 }
